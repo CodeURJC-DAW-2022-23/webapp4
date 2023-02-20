26b4d8a4ca64e834db8a2a125389d40914e9cfb4
scuadrosf <sercua.flores@gmail.com>
2023-02-20 13:27:56 +0100

    nueva arquitectura

---
+++ idealtrip/src/main/java/com/idealtrip/idealtrip/IdealtripApplication.java
@@ -0,0 +1,13 @@
+package com.idealtrip.idealtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealtripApplication.class, args);
+	}
+
+}
