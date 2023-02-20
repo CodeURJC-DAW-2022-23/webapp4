717e3c482f2536de636b544de5d410d85c7020d0
scuadrosf <sercua.flores@gmail.com>
2023-02-20 18:08:05 +0100

    Eliminada y creada arquitectura SpringBoot

---
+++ Backend/idealTrip/src/test/java/com/idealtrip/idealTrip/IdealTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idealtrip.idealTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
