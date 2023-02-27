e08de270a8355fe9dff6abc857a2b1b42eeb8cd1
JrgDfrn <102818224+JrgDfrn@users.noreply.github.com>
2023-02-27 12:33:03 +0100

    newsletter

---
+++ Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/model/Newsletter.java
@@ -0,0 +1,28 @@
+package com.idealtrip.idealTrip.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "newsletter")
+public class Newsletter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String email;
+
+    public Newsletter() {
+    }
+
+    public Newsletter(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
