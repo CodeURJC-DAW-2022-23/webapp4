295a6e27b5226ee7b3c11a3af5969414078bfe93
JrgDfrn <102818224+JrgDfrn@users.noreply.github.com>
2023-02-27 13:35:50 +0100

    newsletter 2.0

---
+++ Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/repository/NewsletterRepository.java
@@ -0,0 +1,12 @@
+package com.idealtrip.idealTrip.repository;
+
+import com.idealtrip.idealTrip.model.Newsletter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface NewsletterRepository extends JpaRepository<Newsletter, Long> {
+    Optional<Newsletter> findByName(String email);
+
+    Optional<Newsletter> findByEmail(String email);
+}
