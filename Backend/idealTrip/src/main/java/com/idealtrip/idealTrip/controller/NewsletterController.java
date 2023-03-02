31b6bf7bf0a973d5d854c45333b02127fc1f2edf
scuadrosf <sercua.flores@gmail.com>
2023-03-02 20:09:29 +0100

    Newsletter controller

---
+++ Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/NewsletterController.java
@@ -0,0 +1,36 @@
+package com.idealtrip.idealTrip.controller;
+
+
+import java.net.URI;
+import java.net.http.HttpResponse.ResponseInfo;
+
+import org.aspectj.apache.bcel.classfile.NestHost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.idealtrip.idealTrip.model.Newsletter;
+import com.idealtrip.idealTrip.service.NewsletterService;
+
+@Controller
+public class NewsletterController {
+    @Autowired
+    private NewsletterService newsletterService;
+
+    @PostMapping("/newsletter")
+    public String createNewsletter (Model model, Newsletter newsletter){
+        String email = newsletter.getEmail();
+        newsletter.setEmail(email);
+        newsletterService.save(newsletter);
+        // model.addAttribute("newsletterId", newsletter.getId());
+        return "/services";
+    }
+    
+}
--- Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/ServiceController.java
+++ Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/ServiceController.java
@@ -1,14 +1,53 @@
 package com.idealtrip.idealTrip.controller;
 
+import java.security.Principal;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.idealtrip.idealTrip.model.Destination;
+import com.idealtrip.idealTrip.repository.DestinationRepository;
+import com.idealtrip.idealTrip.service.DestinationService;
+
+// import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 @Controller
+// @RequestMapping("/services")
 public class ServiceController {
+    @Autowired
+    private DestinationService destinationService;
+
+    @Autowired
+    private DestinationRepository destinationRepository;
     
+    // @ModelAttribute
+    // public void addAttributes(Model model, HttpServletRequest request){
+    //     Principal principal = request.getUserPrincipal();
+
+    //     if (principal != null){
+    //         model.addAttribute("logged", true);
+    //         model.addAttribute("userName", principal.getName());
+    //         model.addAttribute("admin", request.isUserInRole("ADMIN"));
+    //     }else{
+    //         model.addAttribute("logged", false);
+    //     }
+    // }   
+
     @GetMapping("/services")
-     public String services(Model model){
-         return "services";
-     }
+    public String listarNombresDestinos(Model model){
+        Page<Destination> destination = destinationRepository.findAll(PageRequest.of(0,3));
+        model.addAttribute("services", destination.getContent());
+        return "services";
+   }
+
+    
 }
