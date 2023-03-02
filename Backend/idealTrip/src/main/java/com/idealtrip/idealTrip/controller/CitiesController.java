e1a37b4d5e62b70d2973fdd611df53e8a9f33ada
DavidMorenoo <davidmorenom17@gmail.com>
2023-03-02 20:30:29 +0100

    Fixed URls

--- Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/CitiesController.java
+++ Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/CitiesController.java
@@ -13,27 +13,27 @@ public class CitiesController {
     return "PARIS/informationParis";
   }
 
-  @GetMapping("/PARIS/informationParis.html")
+  @GetMapping("/PARIS/informationParis")
   public String servicesParisInfo(Model model) {
     return "PARIS/informationParis";
   }
 
-  @GetMapping("/PARIS/cateringParis.html")
+  @GetMapping("/PARIS/cateringParis")
   public String servicesParisCatering(Model model) {
     return "PARIS/cateringParis";
   }
 
-  @GetMapping("/PARIS/tourismParis.html")
+  @GetMapping("/PARIS/tourismParis")
   public String servicesParistourism(Model model) {
     return "PARIS/tourismParis";
   }
 
-  @GetMapping("/PARIS/reviewParis.html")
+  @GetMapping("/PARIS/reviewParis")
   public String servicesParisreview(Model model) {
     return "PARIS/reviewParis";
   }
 
-  @GetMapping("/PARIS/paris1.html")
+  @GetMapping("/PARIS/paris1")
   public String servicesParisHouse(Model model) {
     return "PARIS/paris1";
   }
@@ -46,27 +46,27 @@ public class CitiesController {
     return "BANGKOK/informationBangkok";
   }
 
-  @GetMapping("/BANGKOK/informationBangkok.html")
+  @GetMapping("/BANGKOK/informationBangkok")
   public String servicesBangkokInfo(Model model) {
     return "BANGKOK/informationBangkok";
   }
 
-  @GetMapping("/BANGKOK/cateringBangkok.html")
+  @GetMapping("/BANGKOK/cateringBangkok")
   public String servicesBangkokCatering(Model model) {
     return "BANGKOK/cateringBangkok";
   }
 
-  @GetMapping("/BANGKOK/tourismBangkok.html")
+  @GetMapping("/BANGKOK/tourismBangkok")
   public String servicesBangkoktourism(Model model) {
     return "BANGKOK/tourismBangkok";
   }
 
-  @GetMapping("/BANGKOK/reviewBangkok.html")
+  @GetMapping("/BANGKOK/reviewBangkok")
   public String servicesBangkokreview(Model model) {
     return "BANGKOK/reviewBangkok";
   }
 
-  @GetMapping("/BANGKOK/bangkok1.html")
+  @GetMapping("/BANGKOK/bangkok1")
   public String servicesBangkokHouse(Model model) {
     return "BANGKOK/bangkok1";
   }
