130286b9f7f90f26dca0bc62a79114418509e66a
AdriPdr <102820600+AdriPdr@users.noreply.github.com>
2023-03-12 21:59:52 +0100

    continue BarChart

--- Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/IndexController.java
+++ Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/IndexController.java
@@ -26,6 +26,7 @@ import com.idealtrip.idealTrip.model.User;
 import com.idealtrip.idealTrip.service.DestinationService;
 import com.idealtrip.idealTrip.service.UserService;
 import com.idealtrip.idealTrip.service.ReviewService;
+
 @Controller
 public class IndexController {
 
@@ -40,49 +41,44 @@ public class IndexController {
 
   User currentUser;
 
+  @ModelAttribute
+  public void addAttributes(Model model, HttpServletRequest request) {
+    Principal principal = request.getUserPrincipal();
 
-    @ModelAttribute
-    public void addAttributes(Model model, HttpServletRequest request){
-        Principal principal = request.getUserPrincipal();
+    if (principal != null) {
+      userService.findByEmail(principal.getName()).ifPresent(us -> currentUser = us);
+      model.addAttribute("logged", true);
+      model.addAttribute("userName", principal.getName());
+      model.addAttribute("admin", request.isUserInRole("ADMIN"));
+      model.addAttribute("curretUser", currentUser);
 
-        if (principal != null){
-            userService.findByEmail(principal.getName()).ifPresent(us->currentUser=us);
-            model.addAttribute("logged", true);
-            model.addAttribute("userName", principal.getName());
-            model.addAttribute("admin", request.isUserInRole("ADMIN"));
-            model.addAttribute("curretUser", currentUser);
-            
-        }else{
-            model.addAttribute("logged", false);
-        }
+    } else {
+      model.addAttribute("logged", false);
     }
-
-
+  }
 
   @GetMapping("/index")
-  public String SearchCountries(Model model){
-      model.addAttribute("services", destinationService.findAll());
-      return "index";
-    }
+  public String SearchCountries(Model model) {
+    model.addAttribute("services", destinationService.findAll());
+    return "index";
+  }
 
   @GetMapping("/")
-    public String searchCountriesInd(Model model) {
-      model.addAttribute("services", destinationService.findAll());
-      return "index";
-    }
+  public String searchCountriesInd(Model model) {
+    model.addAttribute("services", destinationService.findAll());
+    return "index";
+  }
 
+  @GetMapping("/rating")
 
-    @GetMapping("/rating")
-
-    public String ratingDestination(Model model){
+  public String ratingDestination(Model model) {
     List<Destination> destinationList = destinationService.findAll();
     Map<String, Double> totalRatingMap = new HashMap<>();
-      for(Destination destination : destinationList) {
-        Double totalRating = reviewService.getTotalRatingForDestination(destination);
-        totalRatingMap.put(destination.getNameDestination(), totalRating);
-      }
-      model.addAttribute("totalRatingMap", totalRatingMap);
-      return "rating";
-    
+    for (Destination destination : destinationList) {
+      Double totalRating = reviewService.getTotalRatingForDestination(destination);
+      totalRatingMap.put(destination.getNameDestination(), totalRating);
     }
+    model.addAttribute("totalRatingMap", totalRatingMap); 
+    return "rating";
+  }
 }
