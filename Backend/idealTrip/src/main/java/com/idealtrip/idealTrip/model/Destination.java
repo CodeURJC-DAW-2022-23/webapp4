659c53165361d9490f75731e063c5489c384a82a
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2023-03-27 22:44:58 +0200

    Update Destination.java

--- Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/model/Destination.java
+++ Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/model/Destination.java
@@ -3,7 +3,6 @@ package com.idealtrip.idealTrip.model;
 import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,61 +10,68 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
-//import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 
 
 @Entity(name = "destinationTable")
 public class Destination {
 
+    public interface Basico {}
+    public interface Avanzada {}
+
+    public interface Todo extends Basico, Avanzada {}
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(Basico.class)
     private Long id;
 
     @Lob
     @JsonIgnore
     private Blob titleImageFile;
+    @JsonView(Basico.class)
     private String titleImage;
 
     @Column(name = "contentDestination", columnDefinition = "TEXT")
+    @JsonView(Basico.class)
     private String contentDestination;
-
+    @JsonView(Basico.class)
     private String nameDestination;
 
     @OneToOne(mappedBy = "destination", cascade = CascadeType.ALL)
+    @JsonView(Avanzada.class)
     private House house;
 
     @OneToMany(mappedBy = "destination", cascade = CascadeType.ALL)
+    @JsonView(Avanzada.class)
     private List<Review> reviews = new ArrayList<>();
 
     @OneToMany(mappedBy = "destination", cascade = CascadeType.ALL)
+    @JsonView(Avanzada.class)
     private List<Catering> catering;
 
     @OneToMany(mappedBy = "destination", cascade = CascadeType.ALL)
+    @JsonView(Avanzada.class)
     private List<Tourism> tourism;
 
+    @JsonView(Basico.class)
     private float price;
 
     public Destination() {
     }
 
-    public Destination(String contentDestination,String nameDestination, float price, Blob titleImageFile){
+    public Destination(Long id, String contentDestination, String nameDestination,
+            List<Review> reviews, float price) {
+        this.id = id;
         this.contentDestination = contentDestination;
         this.nameDestination = nameDestination;
+        this.reviews = reviews;
         this.price = price;
-        this.titleImageFile = titleImageFile;
     }
-    // public Destination(String contentDestination, String nameDestination,
-    //         List<Review> reviews, float price) {
-    //     //this.id = id;
-    //     this.contentDestination = contentDestination;
-    //     this.nameDestination = nameDestination;
-    //     this.reviews = reviews;
-    //     this.price = price;
-    // }
 
     public Blob getTitleImageFile() {
         return titleImageFile;
