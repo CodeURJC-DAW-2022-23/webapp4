cd2276f62acd03e485072cdbac7683fc1c367d3f
AdriPdr <102820600+AdriPdr@users.noreply.github.com>
2023-03-27 21:24:57 +0200

    fixApi
    Co-Authored-By: JrgDfrn <102818224+JrgDfrn@users.noreply.github.com>
    Co-Authored-By: scuadrosf <101071401+scuadrosf@users.noreply.github.com>

--- Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/rest/CitiesRestController.java
+++ Backend/idealTrip/src/main/java/com/idealtrip/idealTrip/controller/rest/CitiesRestController.java
@@ -11,21 +11,11 @@ import com.idealtrip.idealTrip.controller.DTOS.ReviewDTO;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.idealtrip.idealTrip.model.*;
 import com.idealtrip.idealTrip.service.*;
-
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/destinations")
@@ -45,18 +35,18 @@ public class CitiesRestController {
 
     @GetMapping("/")
     @JsonView(Destination.Basico.class)
-    public Collection<Destination> getDestinations() {
+    public Collection<Destination> getDestinations(){
         return destinations.findAll();
     }
 
     @GetMapping("/{id}")
     @JsonView(Destination.Basico.class)
-    public ResponseEntity<Destination> getDestination(@PathVariable long id) {
+    public ResponseEntity<Destination> getDestiantion(@PathVariable long id){
         Optional<Destination> dest = destinations.findById(id);
-        if (dest.isPresent()) {
+        if (dest.isPresent()){
             Destination destaux = dest.get();
             return new ResponseEntity<>(destaux, HttpStatus.OK);
-        } else {
+        }else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -83,63 +73,24 @@ public class CitiesRestController {
 
     @GetMapping("/catering/{id}")
     @JsonView(Catering.Basic.class)
-    public Collection<Catering> getCatering(@PathVariable long id) {
+    public Collection<Catering> getCatering(@PathVariable long id){
         return catering.findByDestination(id);
     }
-
     @GetMapping("/tourism/{id}")
     @JsonView(Tourism.Basic.class)
-    public Collection<Tourism> getTourism(@PathVariable long id) {
+    public Collection<Tourism> getTourism(@PathVariable long id){
         return tourism.findByDestinationId(id);
     }
-
     @GetMapping("/house/{id}")
     @JsonView(House.Basic.class)
-    public Collection<House> getHouse(@PathVariable long id) {
+    public Collection<House> getHouse(@PathVariable long id){
         return house.findByDestinationName(destinations.findById(id).get().getNameDestination());
     }
-
-    //Mostrar review concreta
-    @GetMapping("/reviews/{id}/{idreview}")
-    @JsonView(Review.Basic.class)
-    public Optional<Review> getReviews(@PathVariable long id, @PathVariable long idreview) {
-        review.findByDestination(id);
-        return this.review.findById(idreview);
-    }
-    @DeleteMapping("/reviews/{id}/{idreview}")
-    @JsonView(Review.Basic.class)
-    public ResponseEntity<Review> deleteReviews(@PathVariable long id, @PathVariable long idreview){
-        review.findByDestination(id);
-        Optional<Review> reviDel = this.review.findById(idreview);
-        if (reviDel.isPresent()) {
-            Review revidel = reviDel.get();
-            review.deleteById(idreview);
-            return new ResponseEntity<>(revidel ,HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-    }
-
-    
-
-    @PostMapping("/reviews/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    @JsonView(Review.Basic.class)
-    public Review createReview(@PathVariable Long id, @RequestBody ReviewDTO newreview) {
-
-        Destination currentDestination = destinations.findDestinationById(id).orElse(null);
-        Review auxreview = new Review(currentUser, currentDestination, newreview.getReviewTitle(), newreview.getRating(), newreview.getReviewContent());
-        review.save(auxreview);
-        return auxreview;
-    }
-
     @GetMapping("/reviews/{id}")
     @JsonView(Review.Basic.class)
     public List<Review> getReview(@PathVariable Long id, Pageable page){
         return review.findByDestination(id, page).getContent();
     }
-
     //show specific review
     @GetMapping("/reviews/{id}/{idreview}")
     @JsonView(Review.Basic.class)
@@ -162,10 +113,12 @@ public class CitiesRestController {
 
     }
 
+    
+
     @PostMapping("/reviews/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     @JsonView(Review.Basic.class)
-    public Review createReviews(@PathVariable Long id, @RequestBody ReviewDTO newreview) {
+    public Review createReview(@PathVariable Long id, @RequestBody ReviewDTO newreview) {
 
         Destination currentDestination = destinations.findDestinationById(id).orElse(null);
         Review auxreview = new Review(currentUser, currentDestination, newreview.getReviewTitle(), newreview.getRating(), newreview.getReviewContent());
