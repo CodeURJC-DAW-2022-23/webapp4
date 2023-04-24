98835e726ff00d7625a513b22b686d48ec9bdd41
JrgDfrn <102818224+JrgDfrn@users.noreply.github.com>
2023-04-24 04:45:40 +0200

    fixed compile

--- allCode/frontend/src/app/components/destination/destination.component.ts
+++ allCode/frontend/src/app/components/destination/destination.component.ts
@@ -10,19 +10,19 @@ import { HttpClient } from '@angular/common/http';
 export class DestinationComponent {
 
   destinations:string[]=[];
-
   constructor(private httpClient:HttpClient){}
 
+
   search(){
-    this.destinations=[];
-    let url="/api/destinations/";
-    this.httpClient.get(url).subscribe(
-      response => {
-        let data:any=response;
-        console.log(response)
-      },
-      error => console.error(error)
-    )
+    // this.destinations=[];
+    // let url="/api/destinations/";
+    // this.httpClient.get(url).subscribe(
+    //   response => {
+    //     let data:any=response;
+    //     console.log(response)
+    //   },
+    //   error => console.error(error)
+    // )
   }
 
 }
