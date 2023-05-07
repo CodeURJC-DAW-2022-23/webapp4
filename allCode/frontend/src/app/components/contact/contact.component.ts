41a596690536c766f9caf280552b8107c17f793e
JrgDfrn <102818224+JrgDfrn@users.noreply.github.com>
2023-05-07 17:12:12 +0200

    send emails

--- allCode/frontend/src/app/components/contact/contact.component.ts
+++ allCode/frontend/src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
+  //title = 'enviodeCorreos';
+  datos: FormGroup;
+  constructor(private httpclien:HttpClient) {
+    this.datos = new FormGroup({
+      correo: new FormControl('',[Validators.required, Validators.email]),
+      nombre: new FormControl('',[Validators.required]),
+      asunto: new FormControl('',[Validators.required]),
+      mensaje: new FormControl('',[Validators.required])
+    })
+  }
+  sendEmail(){
+    let params = {
+      email:this.datos.value.correo,
+      name:this.datos.value.nombre,
+      asunto:this.datos.value.asunto,
+      mensaje:this.datos.value.mensaje
+    }
+    this.httpclien.post('https://localhost:8443',params).subscribe(resp=>{
+      console.log(resp)
+    })
+  }
 }
