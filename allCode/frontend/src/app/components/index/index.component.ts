f2250111ce8f4fb8cebe779a215d2dc3a0cf02ad
AdriPdr <102820600+AdriPdr@users.noreply.github.com>
2023-05-07 18:29:17 +0200

    fixed search

--- allCode/frontend/src/app/components/index/index.component.ts
+++ allCode/frontend/src/app/components/index/index.component.ts
@@ -21,11 +21,71 @@ export class IndexComponent implements OnInit {
     this.destinationService.getDestinations().subscribe((response) => {
       this.destinations = response.content;
     }); 
+    this.prueba(); 
   }
 
-   irAPagina() {
+  irAPagina() {
     var menuDesplegable = document.getElementById("menuDesplegable") as HTMLSelectElement;
     var valorSeleccionado = menuDesplegable?.options[menuDesplegable.selectedIndex].value;
     window.location.href = valorSeleccionado;
-    }
+  }
+
+  prueba() {
+    //   $(window).on("scroll", function () {
+    //     var scroll = $(window).scrollTop();
+
+    //     if (scroll >= 80) {
+    //       $("#site-header").addClass("nav-fixed");
+    //     } else {
+    //       $("#site-header").removeClass("nav-fixed");
+    //     }
+    //   });
+
+    //   //Main navigation Active Class Add Remove
+    //   $(".navbar-toggler").on("click", function () {
+    //     $("header").toggleClass("active");
+    //   });
+    //   $(document).on("ready", function () {
+    //     if ($(window).width() > 991) {
+    //       $("header").removeClass("active");
+    //     }
+    //     $(window).on("resize", function () {
+    //       if ($(window).width() > 991) {
+    //         $("header").removeClass("active");
+    //       }
+    //     });
+    //   });
+    window.addEventListener("scroll", function () {
+      const scroll = window.pageYOffset;
+
+      const header = document.getElementById("site-header") as HTMLElement;
+
+      if (scroll >= 80) {
+        header.classList.add("nav-fixed");
+      } else {
+        header.classList.remove("nav-fixed");
+      }
+    });
+    const navbarToggler = document.querySelector(".navbar-toggler") as HTMLElement;
+
+    navbarToggler.addEventListener("click", function () {
+      const header = document.querySelector("header") as HTMLElement;
+      header.classList.toggle("active");
+    });
+
+    window.addEventListener("load", function () {
+      if (window.innerWidth > 991) {
+        const header = document.querySelector("header") as HTMLElement;
+        header.classList.remove("active");
+      }
+
+      window.addEventListener("resize", function () {
+        if (window.innerWidth > 991) {
+          const header = document.querySelector("header") as HTMLElement;
+          header.classList.remove("active");
+        }
+      });
+    });
+
+  }
 }
