7e4c8d0a464b0a57a8abd7585d83d599ae4bea46
JrgDfrn <102818224+JrgDfrn@users.noreply.github.com>
2023-05-08 04:12:51 +0200

    end of newsletter

--- allCode/frontend/src/app/components/footer/footer.component.ts
+++ allCode/frontend/src/app/components/footer/footer.component.ts
@@ -1,11 +1,29 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  constructor (){}
+  correo: FormGroup;
+  constructor (private httpclien:HttpClient){
+    this.correo = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    })
+  }
+  sendNewsletter(){
+
+    let params = {
+      email:this.correo.value.email,
+    }
+    this.httpclien.post('/api/newsletter/', params)
+      .subscribe(resp => {
+        console.log(resp);
+      }, err => {
+        console.log(err);
+      });
+  }
 }
